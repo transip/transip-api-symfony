b9a215b5c0867be3f27d2c308b2dd53e3c9d3df5
Sem Schilder <sem@tropical.email>
2021-10-26 14:17:56 +0200

    Add gitlab-ci

---
+++ ci/deploy_to_github.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [ -z "$GITHUB_USERNAME" ] ; then
+  echo "we need a github username in order to proceed";
+  exit 1;
+fi
+if [ -z "$GITHUB_TOKEN" ] ; then
+  echo "we need a github token in order to proceed";
+  exit 1;
+fi
+if [ -z "$CI_REPOSITORY_URL" ] ; then
+  echo "we need a repository url to proceed";
+  exit 1;
+fi
+
+# force a full clone, instead of expecting a non shallow cloned repository
+git clone $CI_REPOSITORY_URL /tmp/repository
+cd /tmp/repository
+
+# Remove the default origin/HEAD -> origin/master ref
+# as github shows this as new branch
+git remote set-head origin -d
+
+git push --prune https://${GITHUB_USERNAME}:${GITHUB_TOKEN}@github.com/transip/transip-api-symfony.git +refs/remotes/origin/*:refs/heads/* +refs/tags/*:refs/tags/*
