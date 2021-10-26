8f67856c657ea8a041dca5bb4c28ce922982f76d
Sem Schilder <sem@tropical.email>
2021-10-26 09:27:20 +0200

    Remove unused use

---
+++ src/Exception/InvalidTokenException.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Transip\Bundle\RestApi\Exception;
+
+use Throwable;
+
+class InvalidTokenException extends ApiException
+{
+    public function __construct()
+    {
+        parent::__construct('Access token is invalid.');
+    }
+}
