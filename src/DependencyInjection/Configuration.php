652f6fd5c69b147bdb6ef9c11a0fd737b516eddd
Sem Schilder <sem@tropical.email>
2021-10-28 16:44:02 +0200

    Add config to right position

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -18,28 +18,28 @@ class Configuration implements ConfigurationInterface
 
         $treeBuilder->getRootNode()
             ->children()
-                ->scalarNode('endpoint')
-                    ->info('If this value is not provided, the API client will use the default endpoint.')
-                ->end()
-                ->scalarNode('token')
-                    ->info('Use this to authenticate the API client with a pre-defined access token.')
-                ->end()
-                ->arrayNode('authentication')
-                    ->children()
-                        ->scalarNode('username')
-                            ->info('Your TransIP username.')
-                        ->end() // Username
-                        ->scalarNode('privateKey')
-                            ->info('The private key generated from the Control Panel.')
-                        ->end() // privateKey
-                    ->end()
-                ->end() // authentication
                 ->arrayNode('options')
                     ->children()
                         ->arrayNode('http_plugins')
                             /* Add cast for simple array list */
                             ->scalarPrototype()->end()
                         ->end() // http_plugins
+                        ->scalarNode('endpoint')
+                            ->info('If this value is not provided, the API client will use the default endpoint.')
+                        ->end() // endpoint
+                        ->scalarNode('token')
+                            ->info('Use this to authenticate the API client with a pre-defined access token.')
+                        ->end() // token
+                        ->arrayNode('authentication')
+                            ->children()
+                                ->scalarNode('username')
+                                    ->info('Your TransIP username.')
+                                ->end() // Username
+                                ->scalarNode('privateKey')
+                                    ->info('The private key generated from the Control Panel.')
+                                ->end() // privateKey
+                            ->end()
+                        ->end() // authentication
                     ->end()
                 ->end() // options
             ->end()
